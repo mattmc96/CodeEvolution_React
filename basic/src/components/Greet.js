f277de4abf33ebb1d57071d57ba9bddef10fae1e
vaibhav dhiman <princedhiman70@gmail.com>
2020-07-09 16:11:13 +0530

    class and function based done

---
+++ basic/src/components/Greet.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// function Greet() {
+//     return <h2>Hello Vaibhav</h2>
+// }
+
+const Greet = () => <h3>helllo function component</h3>
+
+export default Greet;
---
+++ basic/src/components/Welcome.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Welcome extends Component {
+    render() {
+
+        return (
+            <div>Heloo class Component</div>
+        );
+    }
+
+}
+
+
+export default Welcome;
