62c36423759777a27c08e7176c8d4fb851fd6b8b
vaibhav dhiman <princedhiman70@gmail.com>
2020-07-20 19:08:12 +0530

    hooks as component did mount

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
-import HooksCounter from './components/HooksCounter';
+//import HooksCounter from './components/HooksCounter';
 //import HookCounterTwo from './components/HookCounterTwo';
 // import HookCounterThree from './components/HookCounterThree';
 //import ClassCounterOne from './components/ClassCounterOne';
+import HookMouse from './components/HookMouse';
 
 function App() {
   return (
     <div className="App">
-        <HooksCounter />      
+        <HookMouse />      
          {/* <HookCounterTwo/> */}
          {/* <HookCounterThree/> */}
          {/* <ClassCounterOne/> */}
--- hooks/src/components/HooksCounter.js
+++ hooks/src/components/HooksCounter.js
@@ -3,13 +3,16 @@ import React, { useState, useEffect } from 'react'
 export default function HooksCounter() {
     
     const [count, setCount] = useState(0)
+    const [name, setName] = useState('')
 
     useEffect(() => {
+        console.log('useEffect - Updating doc title')
         document.title = `You Cliked ${count} Times !` 
-    })
+    }, [count])
 
     return (
         <div>
+            <input type='text' value={name} onChange={e => setName(e.target.value)} />
             <button onClick={()=> setCount(count+1)}>Count {count}</button>            
         </div>
     )
