4c7862fd30a58a38f5e95c54ea92f45df7b074f8
vaibhav dhiman <princedhiman70@gmail.com>
2020-07-20 15:45:05 +0530

    hook counter added

---
+++ hooks/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import HooksCounter from './components/HooksCounter';
+
+function App() {
+  return (
+    <div className="App">
+        <HooksCounter />      
+    </div>
+  );
+}
+
+export default App;
