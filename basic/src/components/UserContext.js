8e9ca3c9c4a9aa2b4a65800cbd69ec441b9dc39a
vaibhav dhiman <princedhiman70@gmail.com>
2020-07-20 14:46:27 +0530

    basic done

--- basic/src/components/UserContext.js
+++ basic/src/components/UserContext.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 const UserContext = React.createContext()
 
+// set default value below .. remove contex from app.js to use this
+//const UserContext = React.createContext("Vaibhav")
+
 const UserProvider = UserContext.Provider
 const UserConsumer = UserContext.Consumer
 
